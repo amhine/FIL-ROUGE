d3090d7a06e1021db27b7cae827cffdd06ef2dba
amhine <nihadamhine@gmail.com>
2025-04-13 14:25:22 +0100

    pagination page restaurant

--- rouge/resources/views/restaurant.blade.php
+++ rouge/resources/views/restaurant.blade.php
@@ -321,6 +321,32 @@
         
         </div>
     </section>
+    <!-- Pagination -->
+    <div class="flex justify-center items-center mt-10 mb-10">
+        <nav class="relative z-0 inline-flex rounded-md shadow-sm -space-x-px" aria-label="Pagination">
+        
+        <a href="#" class="relative inline-flex items-center px-3 py-2 rounded-l-md border border-gray-300 bg-[#C02626] text-white text-sm font-medium hover:bg-[#A42020]">
+            <span class="sr-only">Précédent</span>
+            <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+            <path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd" />
+            </svg>
+        </a>
+        
+        <a href="#" class="bg-white border-gray-300 text-gray-500 hover:bg-[#FECACA] relative inline-flex items-center px-4 py-2 border text-sm font-medium">1</a>
+        <a href="#" class="bg-white border-gray-300 text-gray-500 hover:bg-[#FECACA] relative inline-flex items-center px-4 py-2 border text-sm font-medium">2</a>
+        <a href="#" class="bg-white border-gray-300 text-gray-500 hover:bg-[#FECACA] relative inline-flex items-center px-4 py-2 border text-sm font-medium">3</a>
+        
+        <a href="#" class="relative inline-flex items-center px-3 py-2 rounded-r-md border border-gray-300 bg-[#C02626] text-white text-sm font-medium hover:bg-[#A42020]">
+            <span class="sr-only">Suivant</span>
+            <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+            <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
+            </svg>
+        </a>
+        </nav>
+    </div>
+  
+   
+
 
 
 
