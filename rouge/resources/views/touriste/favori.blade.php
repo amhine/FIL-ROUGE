3cbd4e687654a7931cb647e05a8307eac242bbe4
amhine <nihadamhine@gmail.com>
2025-04-24 12:28:09 +0100

    ajouter des annonce des resteaux au page favori

--- rouge/resources/views/touriste/favori.blade.php
+++ rouge/resources/views/touriste/favori.blade.php
@@ -121,7 +121,50 @@
         @endif
         
     </div>
-  
+    <section class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 p-4">
+        @if($restaurants->count() > 0)
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+            @foreach($restaurants as $resteaux)
+            <div class="bg-white rounded-3xl shadow-lg overflow-hidden flex flex-col">
+                <div class="relative h-48 overflow-hidden">
+                    <img src="{{ $resteaux->image }}" alt="Restaurant" class="w-full h-full object-cover">
+                    <div class="absolute top-4 right-4 bg-green-600 text-white px-3 py-1 rounded-full text-sm font-bold">
+                        {{ $resteaux->prix }} DH/nuit
+                    </div>
+                    <div class="absolute bottom-4 left-4 bg-green-500 text-white px-3 py-1 rounded-full text-xs font-bold">
+                        {{ $resteaux->type_cuisine }}
+                    </div>
+                    <form action="{{ route('favoris.restaurant', $resteaux->id) }}" method="POST" class="absolute top-4 left-4">
+                        @csrf
+                        <button type="submit" class="favorite-btn text-white bg-gray-800 bg-opacity-60 p-2 rounded-full hover:bg-red-500 transition">
+                            <i class="fas fa-heart {{ $resteaux->isFavorited() ? 'text-red-500' : 'text-white' }}"></i>
+                        </button>
+                    </form>
+                    
+
+              
+                </div>
+                <div class="p-6 flex-grow">
+                    <h3 class="text-xl font-bold text-gray-800 mb-2">{{ $resteaux->nom_resteau }}</h3>
+                    <div class="flex items-center text-gray-600 mb-2">
+                        <i class="fas fa-map-marker-alt mr-2 text-red-600"></i>
+                        <span>{{ $resteaux->localisation }}</span>
+                    </div>
+                    <p class="text-gray-600 mb-4">{{ $resteaux->description }}</p>
+                    <button class="w-full mt-4 bg-red-600 text-white py-2 rounded-lg font-medium hover:opacity-90 transition duration-300 transform hover:scale-105">
+                        Réserver
+                    </button>
+                </div>
+            </div>
+            @endforeach
+        </div>
+        
+        @else
+        <div class="text-center py-10">
+            <p class="text-gray-500 text-xl">Aucune annonce disponible.</p>
+        </div>
+        @endif
+    </section>
      <!-- Footer -->
      <footer class="bg-[#c92424] text-white py-16">
         <div class="container mx-auto px-8">
